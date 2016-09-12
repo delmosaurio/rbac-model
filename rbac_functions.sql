1f3fc224249a8b5233053fd48b56a45bf013b556
Delmo <delmosaurio@gmail.com>
2016-09-11 21:26:44 -0300

    add session fo express-session

--- rbac_functions.sql
+++ rbac_functions.sql
@@ -1,3 +1,12 @@
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+  "sess" json NOT NULL,
+  "expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+
 CREATE OR REPLACE FUNCTION bit_xor(
   _value_a integer,
   _value_b integer
